94282d66df66f1e742e16dd704a890968cf9238e
mrsguerri <mattsguerri@gmail.com>
2022-05-20 14:35:55 -0700

    updates

--- main.go
+++ main.go
@@ -1,9 +1,33 @@
 package main
 
-import "practice/quicksort"
+import (
+	"fmt"
+	"practice/quicksort"
+)
 
 func main() {
-	quicksort.Quicksort("aseedf")
+
+	//one way to initialize an array
+	//var input [5]int = [5]int{7, 1, 9, 3, 2}
+
+	//var input[5]int
+	//input = [5]int{7, 1, 9, 3, 2}
+	//can also be assigned partially, rest are default
+	//input = [5]int{7, 1, 9}
+
+	//ellipsis
+	//input := [...]int{7, 1, 9, 3, 2}
+
+	//stylistically preferred
+	input := []int{7, 1, 9, 3, 2}
+
+	fmt.Println("Pre-sorted array")
+	fmt.Println(input)
+
+	fmt.Println("quicksort")
+	sorted := quicksort.Quicksort(input, 0, len(input))
+	fmt.Println(sorted)
 }
 
 //https://go.dev/tour/flowcontrol/1
+//https://www.golangprograms.com/go-language/arrays.html
--- quicksort/quicksort.go
+++ quicksort/quicksort.go
@@ -2,6 +2,14 @@ package quicksort
 
 import "fmt"
 
-func Quicksort(name string) {
-	fmt.Println(name)
+func Quicksort(array []int, start, end int) []int {
+
+	fmt.Println(len(array))
+
+	//Steps for a quicksort:
+	//1. Find a pivot (can be the first element, middle element, last element, or a random element)
+	//2. Put all elements smaller than the pivot to its left, and all elements larger to its right
+
+
+	return array
 }
